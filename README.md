9fe93d6b88637272de6b7dbf6c9d99e6a51cbd96
Takashi Abe <tabe@tabe-ltm.internal.salesforce.com>
2016-03-03 17:19:03 +0900

    Generated a model and a controller for users

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rails-test
+
+I try the Ruby on Rails refer to http://www.atmarkit.co.jp/ait/articles/1402/28/news047.html .
--- hello_world/db/schema.rb
+++ hello_world/db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303075429) do
+ActiveRecord::Schema.define(version: 20160303081423) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.text     "outline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "department"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
